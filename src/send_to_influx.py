2b546d0d3c26fee9faed6371861002baa291cab7
adityakiran1423 <adi.kiran@gmail.com>
2024-06-25 18:25:41 +0530

    fetch from uno and store in idb

---
+++ src/send_to_influx.py
@@ -0,0 +1,10 @@
+# fetch data from uno and store it in influx
+
+def fetch_from_influx() -> None:
+    # stream of data to fetch using serial module
+    pass
+
+def write_to_influx() -> None:
+    #  normal write like how in ardserial.py
+    pass
+
